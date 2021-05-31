7a921f80e8195925d728e7905ea4846abebd5b55
h1-the-swan <jason.portenoy@gmail.com>
2021-05-31 10:48:50 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Card } from "antd";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Footer, Content } = Layout;
+
+interface Project {
+  name: string;
+  description: React.ReactNode | string;
 }
+
+const projects: Project[] = [
+  {
+    name: "Visualizing scholarly influence",
+    description: (
+      <React.Fragment>
+        The scholarly literature is a vast store of formalized human knowledge,
+        interconnected by citations between publications. Looking at these
+        citations is one way to measure the influence of scholarly research.
+        <a
+          href="http://scholar.eigenfactor.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Scholar.eigenfactor.org
+        </a>
+        is a collection of tools I built to use citations to measure and
+        visualize the influence of collections of papers. These collections can
+        represent, for example, individual scholars, or journals, academic
+        departments, or fields of study.
+      </React.Fragment>
+    ),
+  },
+];
+
+const App: React.FC = () => (
+  <div className="App">
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        {projects.map((project) => (
+          <Card title={project.name}>
+            <p>{project.description}</p>
+          </Card>
+        ))}
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
+);
 
 export default App;
